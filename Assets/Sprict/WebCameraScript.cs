5284bf73cbab72a68efff0619972a29c22454e4a
uen0_ <sarasa4869@zoho.com>
2022-04-09 00:17:00 +0900

    カメラの画像を表示させるスクリプト

---
+++ Assets/Sprict/WebCameraScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WebCameraScript : MonoBehaviour
+{
+    [SerializeField] public RawImage _rawImage;
+    WebCamTexture webCamTexture;
+
+    void Start()
+    {
+        webCamTexture = new WebCamTexture();
+        _rawImage.texture = webCamTexture;
+        webCamTexture.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
