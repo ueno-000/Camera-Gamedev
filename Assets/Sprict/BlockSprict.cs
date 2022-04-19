b7007d5260f358494316587c83d0ba8aa4265e43
uen0_ <sarasa4869@zoho.com>
2022-04-19 21:51:58 +0900

    スクリプトの追加

---
+++ Assets/Sprict/BlockSprict.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSprict : MonoBehaviour
+{
+    //[SerializeField] GameObject _effect;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       // Instantiate(_effect,this.transform);
+        Destroy(gameObject);
+    }
+
+}
+
+
