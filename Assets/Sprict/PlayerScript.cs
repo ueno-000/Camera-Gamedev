212f8250081286dda83c6e686ce29cff52511708
uen0_ <sarasa4869@zoho.com>
2022-04-19 21:52:25 +0900

    Scene上の変更

--- Assets/Sprict/PlayerScript.cs
+++ Assets/Sprict/PlayerScript.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] public float _speed;
+
+    Rigidbody2D _rb;
+
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Horizontal"))
-        {
-
-        }
+        // 左右のキー入力により速度を変更する
+        _rb.velocity = new Vector3(Input.GetAxis("Horizontal") * _speed, 0f, 0f);
     }
 }
